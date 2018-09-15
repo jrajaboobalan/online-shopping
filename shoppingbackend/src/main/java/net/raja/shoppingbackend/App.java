057616306f03080a3d94a7cd0a8dc9c62cb67fd1
Raja <jraja1987@gmail.com>
2018-09-16 00:42:58 +0530

    my project setup completed succefully

---
+++ shoppingbackend/src/main/java/net/raja/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.raja.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
